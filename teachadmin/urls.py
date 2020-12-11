ed4258efe7dbb40225b338812dd9fd0db10ca903
Richard Ericson <thedrummer_88@hotmail.com>
2020-12-11 11:33:55 +0800

    Fixed the register functionality with simple interface.

--- teachadmin/urls.py
+++ teachadmin/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 from teachscores import views as teachscore_views
 
@@ -22,4 +25,4 @@ urlpatterns = [
     path('', include('teachscores.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- teachscores/views.py
+++ teachscores/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World!")
 
-class IndexView(TemplateView):
+class IndexView(TemplateView, LoginRequiredMixin):
+    login_url = "/accounts/login/"
     template_name = 'teachscores/index.html'
+
+    redirect_field_name = template_name
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
