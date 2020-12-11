2e43327ca141cd4356c6070b90772b8f7de3d640
Richard Ericson <thedrummer_88@hotmail.com>
2020-12-11 19:47:19 +0800

    Fixed import statement to implement accounts.Teacher into teachscores app

--- teachscores/models.py
+++ teachscores/models.py
@@ -3,27 +3,50 @@ from django.db.models import F
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
-from django_countries.fields import CountryField
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+
+from django_countries.fields import CountryField
 
 import datetime
 
 from .core.models import TimeStampedModel
 
+from accounts.models import Teacher
+
 # Create your models here.
 
-# Put in its own app called accounts?
-"""
-class Teacher(models.Model):
-    # OneToOneField to Django's User model
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Additional Info
-    country = CountryField(blank=True, blank_label='(Select Country)', help_text="Optional")
+"""class School(models.Model):
+    name = models.CharField(
+        max_length=100,
+        help_text="Up to 100 characters"
+    )
+    country = CountryField(
+        blank=True,
+        blank_label="(Select Country)",
+        help_text="Optional."
+    )
+    city = models.CharField(
+        max_length=50,
+        help_text="Up to 50 characters."
+    )
 
-    def __str__(self):
-        return self.user.username
 
-    def get_absolute_url(self):
-        return reverse("teacher_detail", kwargs={"pk": self.pk})
-"""
+class Homeroom(models.Model):
+    name = models.CharField(
+        max_length=30,
+        help_text="Up to 30 characters."
+    )
+    teacher = models.ForeignKey(
+        Teacher,
+        on_delete=models.CASCADE
+    )
+    school = models.ForeignKey(
+        School,
+        on_delete=models.CASCADE
+    )
+ 
+
+
+class Student(models.Model):
+    pass"""
