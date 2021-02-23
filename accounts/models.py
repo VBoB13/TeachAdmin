9741947fd1b043880dff3b65d02da6063d9e6be4
Richard Ericson <thedrummer_88@hotmail.com>
2021-02-23 19:50:26 +0800

    Added some models to the Accounts app that I will finalize tomorrow.

--- accounts/models.py
+++ accounts/models.py
@@ -16,7 +16,10 @@ class Teacher(models.Model):
     # OneToOneField to Django's User model
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # Additional Info
-    country = CountryField(blank=True, blank_label='(Select Country)', help_text="Optional")
+    country = CountryField(
+        blank=True,
+        blank_label='(Select Country)',
+        help_text="Optional")
     career_profile = models.URLField(
         blank=True,
         help_text="URL to your career profile. E.g. LinkedIn, Indeed etc."
@@ -26,7 +29,51 @@ class Teacher(models.Model):
         return str(self.user.username)
 
     def get_absolute_url(self):
-        return reverse("teacher_detail", kwargs={"pk": self.pk})
+        return "/accounts/{}/".format(self.pk)
 
 
+class Organization(models.Model):
+    """
+    This Model will store information about organizations that the teacher enters as past of their experience.
+    """
+    name = models.CharField(
+        verbose_name="Organization name",
+        max_length=50,
+        help_text="Within 50 characters. Please refer to the organization's full English name often found on their website."
+    )
+    country = CountryField(
+        blank=True,
+        blank_label='(Select Country)',
+        help_text="Optional."
+    )
 
+    def __str__(self):
+        return "{} ({})".format(self.name, self.country)
+
+    def get_absolute_url(self):
+        return "/accounts/orgs/{}/".format(self.pk)
+
+
+class Experience(models.Model):
+    """
+    This Model is meant to store an individual teacher's experience(s).
+    """
+    teacher = models.ForeignKey(
+        Teacher, 
+        on_delete=models.CASCADE)
+    start_date = models.DateField(
+        verbose_name="Start date",
+        default=datetime.date.today
+    )
+    end_date = models.DateField(verbose_name="End date")
+    organization = models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE
+    )
+    position = models.CharField(
+        max_length=50,
+        help_text="Within 50 characters."
+    )
+
+    def __str__(self):
+        return "{} at {}".format(self.position, self.organization)
