3aa6cd414531fba1f49dbf19cb3a863cd704417f
Richard Ericson <thedrummer_88@hotmail.com>
2021-02-25 10:34:59 +0800

    Adjusted so that the username gets properly serialized and shown correctly.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import Teacher
 
 class TeacherSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    
     class Meta:
         model = Teacher
         fields = ('id', 'user', 'country', 'career_profile')
