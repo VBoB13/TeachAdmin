b0d3b67eb5208634c0117ebc944621b30b7957b4
Richard Ericson <thedrummer_88@hotmail.com>
2021-03-02 19:06:38 +0800

    Made full country name appear through DRF serializer and django_countries instead of just contry code.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,9 +1,31 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from django_countries.serializers import CountryFieldMixin
+from django_countries.serializer_fields import CountryField
 from .models import Teacher
 
-class TeacherSerializer(serializers.ModelSerializer):
+class TeacherSerializer(CountryFieldMixin, serializers.ModelSerializer):
     user = serializers.StringRelatedField()
+    country = CountryField(name_only=True)
+    career_profile = serializers.URLField(required=False)
     
     class Meta:
         model = Teacher
         fields = ('id', 'user', 'country', 'career_profile')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    teacher = TeacherSerializer(required=False)
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', 'password', 'teacher')
+
+    def create(self, validated_data, instance=None):
+        teacher_data = validated_data.pop('teacher')
+        user = User.objects.create(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        Teacher.objects.update_or_create(user=user, **teacher_data)
+        return user
