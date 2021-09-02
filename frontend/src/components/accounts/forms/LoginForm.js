8dc2fcc42886f8b53e59646419385d1d15ee02eb
Richard Ericson <thedrummer_88@hotmail.com>
2021-09-02 11:36:47 +0800

    Re-factored some minor input field components into OOP style for clarity.

--- frontend/src/components/accounts/forms/LoginForm.js
+++ frontend/src/components/accounts/forms/LoginForm.js
@@ -3,27 +3,14 @@ import { render } from "react-dom";
 import { login } from "../../../helpers/auth";
 
 import TextField from "../forms/fields/TextField";
+import PasswordField from "../forms/fields/PasswordField";
 
 class LoginForm extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      username: "",
-      password: "",
-    };
     // Register Component methods
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
     this.login = this.login.bind(this);
-  }
-
-  handleUsernameChange(event) {
-    this.setState({ username: event.target.value });
-  }
-
-  handlePasswordChange(event) {
-    this.setState({ password: event.target.value });
   }
 
   async login(event) {
@@ -36,18 +23,17 @@ class LoginForm extends Component {
     return (
       <div className="form-content">
         <form method="POST" className="rounded" onSubmit={this.login}>
-          <TextField fieldName="username" />
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              className="form-control"
-              value={this.state.password}
-              onChange={this.handlePasswordChange}
-            />
-          </div>
+          <TextField
+            fieldname="username"
+            id="username"
+            fieldtype="text"
+            autoFocus
+          />
+          <PasswordField
+            fieldname="password"
+            fieldtype="password"
+            id="password"
+          />
           <input type="submit" className="standard-button" value="Login" />
         </form>
       </div>
--- frontend/src/components/accounts/forms/fields/TextField.js
+++ frontend/src/components/accounts/forms/fields/TextField.js
@@ -1,56 +1,8 @@
 import React, { Component } from "react";
+import InputField from "./InputField";
 
-export default class TextField extends Component {
+export default class TextField extends InputField {
   constructor(props) {
     super(props);
-    this.state = {
-      fieldName: `${props.fieldName}`,
-      value: props.defaultValue ?? "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.checkAutoFocus = this.checkAutoFocus.bind(this);
-  }
-
-  handleChange(e) {
-    e.preventDefault();
-    this.setState({ value: e.target.value });
-  }
-
-  checkAutoFocus() {
-    console.log(this.state);
-    if (this.state.fieldName === "username")
-      return (
-        <input
-          type="text"
-          className="form-control"
-          name={this.state.fieldName}
-          id={this.state.fieldName}
-          value={this.state.value}
-          onChange={this.handleChange}
-          required
-          autoFocus
-        ></input>
-      );
-    return (
-      <input
-        type="text"
-        className="form-control"
-        name={this.state.fieldName}
-        value={this.state.value}
-        onChange={this.handleChange}
-      ></input>
-    );
-  }
-
-  render() {
-    return (
-      <div className="form-group">
-        <label htmlFor={this.state.fieldName}>
-          {this.state.fieldName[0].toUpperCase() +
-            this.state.fieldName.slice(1)}
-        </label>
-        {this.checkAutoFocus()}
-      </div>
-    );
   }
 }
