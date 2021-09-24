1853bbb688c12d694ffa7e61ee64060830b1aa68
Richard Ericson <thedrummer_88@hotmail.com>
2021-09-24 17:24:53 +0800

    Added LinkCards for a better look at the HomePage

---
+++ frontend/src/components/cards/cards.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+function BaseCard(props) {
+  return <div className="card-base">{props.children}</div>;
+}
+
+export default function LinkCard(props) {
+  return (
+    <BaseCard>
+      <div className="card-titlebox">{props.title}</div>
+      <img src={props.logo} alt={props.img_alt} className="card-logo" />
+      <div className="card-textbox">
+        <span className="card-text">{props.text}</span>
+      </div>
+    </BaseCard>
+  );
+}
+
+export function DataCard(props) {
+  return "meh";
+}
--- frontend/src/components/homepage/HomePage.js
+++ frontend/src/components/homepage/HomePage.js
@@ -3,20 +3,16 @@ import { render } from "react-dom";
 import { Link } from "react-router-dom";
 
 import LoginMessage from "../accounts/messages/LoginMessage";
+import LinkCard from "../cards/cards";
 
 function AccountLinkButton(props) {
   return (
-    <button className="btn btn-secondary">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        className="bi bi-person-lines-fill"
-        viewBox="0 0 16 16"
-      >
-        <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
-      </svg>{" "}
-      Account ({props.user})
-    </button>
+    <LinkCard
+      title="Who am I?"
+      logo="/static/frontend/svg/working_girl.svg"
+      img_alt="Teacher Working"
+      text="Account Information"
+    />
   );
 }
 
@@ -26,7 +22,7 @@ export default function HomePage(props) {
       <div className="row p-1 my-1">
         <div className="col-4 p-2">
           <Link to="/account/">
-            <AccountLinkButton user={props.user} />
+            <AccountLinkButton />
           </Link>
         </div>
       </div>
