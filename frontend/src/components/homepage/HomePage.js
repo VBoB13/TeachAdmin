3bf4525a6cd5ad75d8667c3b90a0565e42528a78
Richard Ericson <thedrummer_88@hotmail.com>
2021-09-25 15:00:35 +0800

    Re-factored some LinkCard logic on HomePage

--- frontend/src/components/homepage/HomePage.js
+++ frontend/src/components/homepage/HomePage.js
@@ -5,40 +5,28 @@ import { Link } from "react-router-dom";
 import LoginMessage from "../accounts/messages/LoginMessage";
 import LinkCard from "../cards/cards";
 
-function AccountLinkButton(props) {
-  return (
-    <LinkCard
-      title="Who am I?"
-      logo="/static/frontend/svg/working_girl.svg"
-      img_alt="Teacher Working"
-      text="Account Information"
-    />
-  );
-}
-
-function StudentsLinkButton(props) {
-  return (
-    <LinkCard
-      title="Students"
-      logo="/static/frontend/svg/red_beard_avatar.svg"
-      img_alt="Bearded Student"
-      text="Shows all your students"
-    />
-  );
-}
-
 export default function HomePage(props) {
   return (
     <div className="container-fluid">
-      <div className="row p-1 my-1">
-        <div className="col-6 p-2">
+      <div className="row p-1 my-1 justify-content-center align-content-center align-items-center">
+        <div className="col p-2">
           <Link to="/account/">
-            <AccountLinkButton />
+            <LinkCard
+              title="Who am I?"
+              logo="/static/frontend/svg/working_girl.svg"
+              img_alt="Teacher Working"
+              text="Account Information"
+            />
           </Link>
         </div>
-        <div className="col-6 p-2">
+        <div className="col p-2">
           <Link to="/students/">
-            <StudentsLinkButton />
+            <LinkCard
+              title="Students"
+              logo="/static/frontend/svg/red_beard_avatar.svg"
+              img_alt="Bearded Student"
+              text="Shows all your students"
+            />
           </Link>
         </div>
       </div>
