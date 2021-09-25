4c73cd88be3c1fbc3b38990e3b78632b1ac1704d
Richard Ericson <thedrummer_88@hotmail.com>
2021-09-25 15:12:01 +0800

    Re-made the styling of the HomePage since the Bootstrap grid system seemed to give up on me...

--- frontend/src/components/homepage/HomePage.js
+++ frontend/src/components/homepage/HomePage.js
@@ -7,29 +7,27 @@ import LinkCard from "../cards/cards";
 
 export default function HomePage(props) {
   return (
-    <div className="container-fluid">
-      <div className="row p-1 my-1 justify-content-center align-content-center align-items-center">
-        <div className="col p-2">
-          <Link to="/account/">
-            <LinkCard
-              title="Who am I?"
-              logo="/static/frontend/svg/working_girl.svg"
-              img_alt="Teacher Working"
-              text="Account Information"
-            />
-          </Link>
-        </div>
-        <div className="col p-2">
-          <Link to="/students/">
-            <LinkCard
-              title="Students"
-              logo="/static/frontend/svg/red_beard_avatar.svg"
-              img_alt="Bearded Student"
-              text="Shows all your students"
-            />
-          </Link>
-        </div>
-      </div>
-    </div>
+    <main className="link-container">
+      <section className="link-slot">
+        <Link to="/account/">
+          <LinkCard
+            title="Who am I?"
+            logo="/static/frontend/svg/working_girl.svg"
+            img_alt="Teacher Working"
+            text="Account Information"
+          />
+        </Link>
+      </section>
+      <section className="link-slot">
+        <Link to="/students/">
+          <LinkCard
+            title="Students"
+            logo="/static/frontend/svg/red_beard_avatar.svg"
+            img_alt="Bearded Student"
+            text="Shows all your students"
+          />
+        </Link>
+      </section>
+    </main>
   );
 }
