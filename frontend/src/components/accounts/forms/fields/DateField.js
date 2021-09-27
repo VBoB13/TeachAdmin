ef4e133b47f1648d9a33f08adcacc382a65600ff
Richard Ericson <thedrummer_88@hotmail.com>
2021-09-27 21:57:46 +0800

    Significant code improvements as well as simplifications using String.toISOString()

--- frontend/src/components/accounts/forms/fields/DateField.js
+++ frontend/src/components/accounts/forms/fields/DateField.js
@@ -11,44 +11,9 @@ export default class DateField extends InputField {
   handleChange(event) {
     event.preventDefault();
     const chosen_date = new Date(event.target.value);
-    const today = new Date();
+    var date_str = chosen_date.toISOString().split("T")[0];
+    console.log(date_str);
 
-    // If the chosen date is before today
-    if (
-      today.getFullYear() > chosen_date.getFullYear() ||
-      (today.getFullYear() === chosen_date.getFullYear() &&
-        today.getMonth() > chosen_date.getMonth()) ||
-      (today.getFullYear() === chosen_date.getFullYear() &&
-        today.getMonth() === chosen_date.getMonth() &&
-        today.getDate() > chosen_date.getDate())
-    ) {
-      if (chosen_date.getMonth() + 1 < 10) {
-        this.setState({
-          value: `${chosen_date.getFullYear()}-0${
-            chosen_date.getMonth() + 1
-          }-${chosen_date.getDate()}`,
-        });
-      } else {
-        this.setState({
-          value: `${chosen_date.getFullYear()}-${
-            chosen_date.getMonth() + 1
-          }-${chosen_date.getDate()}`,
-        });
-      }
-    } else {
-      if (today.getMonth() + 1 < 10) {
-        this.setState({
-          value: `${today.getFullYear()}-0${
-            today.getMonth() + 1
-          }-${today.getDate()}`,
-        });
-      } else {
-        this.setState({
-          value: `${today.getFullYear()}-${
-            today.getMonth() + 1
-          }-${today.getDate()}`,
-        });
-      }
-    }
+    this.setState({ value: date_str });
   }
 }
