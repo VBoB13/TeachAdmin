7f794f6e5ce589ee1673317af5b7ae122d5ccaa3
Richard E. <thedrummer_88@hotmail.com>
2024-05-06 20:29:27 +0800

    Added School app as well as simple relationships between entities.

---
+++ teachadmin/schools/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django_countries.fields import CountryField
+
+# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=250)
+    country = CountryField(default="SE") # As this project aims to implement into the Swedish education system first
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField(max_length=1000)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=200)
+    desc = models.TextField(max_length=1000)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
--- teachadmin/teachers/models.py
+++ teachadmin/teachers/models.py
@@ -1,3 +1,26 @@
+from datetime import date
+
 from django.db import models
+from django_countries.fields import CountryField
+
+from schools.models import School
 
 # Create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    schools = models.ManyToManyField(
+        School,
+        through="Employment",
+        through_fields=("teacher", "school"),
+    )
+
+
+class Employment(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    start = models.DateField(auto_now_add=True, default=date.today)
+    end = models.DateField(default=models.SET_NULL, null=True)
+    ratio = models.FloatField(default=1.0)
+
+
