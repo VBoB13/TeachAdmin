f2866356602a1258e793a1d273d2cfbf52c30068
Richard E. <thedrummer_88@hotmail.com>
2024-05-07 13:37:18 +0800

    Cleared up some relationship matters for the basics.

--- teachadmin/schools/models.py
+++ teachadmin/schools/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 
 # Create your models here.
@@ -19,4 +20,10 @@ class Subject(models.Model):
     name = models.CharField(max_length=200)
     desc = models.TextField(max_length=1000)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+
+class Staff(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    alias = models.CharField(max_length=50, blank=True, default="")
 
--- teachadmin/teachers/models.py
+++ teachadmin/teachers/models.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from django.db import models
+from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 
 from schools.models import School
@@ -8,7 +9,8 @@ from schools.models import School
 # Create your models here.
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    alias = models.CharField(max_length=50, blank=True, default="")
     schools = models.ManyToManyField(
         School,
         through="Employment",
